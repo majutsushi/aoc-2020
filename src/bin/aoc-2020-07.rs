2fd451a5c7482fd241a47f9645b24e9a80a38e11
Jan Larres <jan@majutsushi.net>
2021-03-21 14:01:14 +1300

    Day 07: Add part 2

--- src/bin/aoc-2020-07.rs
+++ src/bin/aoc-2020-07.rs
@@ -5,7 +5,7 @@ use anyhow::{anyhow, Context, Result};
 use nom::{
     bytes::complete::tag,
     character::complete::{alpha1, char, digit1},
-    combinator::{eof, opt, recognize},
+    combinator::{eof, map_res, opt, recognize},
     error::{convert_error, VerboseError},
     multi::separated_list1,
     sequence::{separated_pair, terminated},
@@ -14,26 +14,37 @@ use nom::{
 
 type NomResult<T, U> = IResult<T, U, VerboseError<T>>;
 
+#[derive(Debug)]
+struct ContainedBag<'a> {
+    count: u32,
+    colour: &'a str,
+}
+
 fn main() -> Result<()> {
     let input = fs::read_to_string("input/07.txt").context("Error reading input file")?;
 
-    // get a map of "outer bag -> inner bags"
-    let outer_to_inner = input
+    // get a map of "outer bag -> inner bags with count"
+    let outer_to_inner: HashMap<&str, Vec<ContainedBag>> = input
         .lines()
         .map(|line| nom_parse(bag_line, line))
-        .collect::<Result<HashMap<_, _>, _>>()?;
+        .collect::<Result<_, _>>()?;
 
-    // invert map to get map of "inner bag -> outer bags"
+    // invert map to get map of "inner bag -> outer bags" with just the colour
     let inner_to_outer = outer_to_inner
         .iter()
         .flat_map(|(outer, inner)| inner.iter().map(|bag| (bag, outer)).collect::<Vec<_>>())
-        .fold(HashMap::new(), |mut map, (&inner, &outer)| {
-            map.entry(inner).or_insert_with(Vec::new).push(outer);
+        .fold(HashMap::new(), |mut map, (inner, &outer)| {
+            map.entry(inner.colour).or_insert_with(Vec::new).push(outer);
             map
         });
 
     let containing_gold = get_containing_bags(&inner_to_outer, "shiny gold", HashSet::new());
     println!("Part 1: {}", containing_gold.len());
+
+    println!(
+        "Part 2: {}",
+        get_contained_bag_count(&outer_to_inner, "shiny gold")
+    );
 
     Ok(())
 }
@@ -52,6 +63,17 @@ fn get_containing_bags<'a>(
     candidates
 }
 
+fn get_contained_bag_count(all_bags: &HashMap<&str, Vec<ContainedBag>>, bag: &str) -> u32 {
+    all_bags
+        .get(bag)
+        .unwrap()
+        .iter()
+        .map(|inner_bag| {
+            inner_bag.count + inner_bag.count * get_contained_bag_count(all_bags, inner_bag.colour)
+        })
+        .sum()
+}
+
 fn nom_parse<I, O, P>(parser: P, input: I) -> Result<O>
 where
     I: Copy + nom::InputLength + std::ops::Deref<Target = str>,
@@ -63,22 +85,23 @@ where
     }
 }
 
-fn bag_line(input: &str) -> NomResult<&str, (&str, Vec<&str>)> {
+fn bag_line(input: &str) -> NomResult<&str, (&str, Vec<ContainedBag>)> {
     terminated(
         separated_pair(bag, tag(" contain "), contained_bags),
         char('.'),
     )(input)
 }
 
-fn contained_bags(input: &str) -> NomResult<&str, Vec<&str>> {
+fn contained_bags(input: &str) -> NomResult<&str, Vec<ContainedBag>> {
     match tag::<_, _, VerboseError<_>>("no other bags")(input) {
         Ok((next, _)) => Ok((next, Vec::new())),
         Err(_) => separated_list1(tag(", "), bag_with_count)(input),
     }
 }
 
-fn bag_with_count(input: &str) -> NomResult<&str, &str> {
-    separated_pair(digit1, char(' '), bag)(input).map(|(next, (_count, colour))| (next, colour))
+fn bag_with_count(input: &str) -> NomResult<&str, ContainedBag> {
+    separated_pair(map_res(digit1, |s: &str| s.parse::<u32>()), char(' '), bag)(input)
+        .map(|(next, (count, colour))| (next, ContainedBag { count, colour }))
 }
 
 fn bag(input: &str) -> NomResult<&str, &str> {
