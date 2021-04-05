7326b95811f5b4421fc8c3e7b59a5e7a58e4edfd
Jan Larres <jan@majutsushi.net>
2021-04-05 16:38:06 +1200

    Day 09: Add part 2

--- src/bin/aoc-2020-09.rs
+++ src/bin/aoc-2020-09.rs
@@ -1,4 +1,5 @@
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
+use itertools::Itertools;
 
 fn main() -> Result<()> {
     let input = std::fs::read_to_string("input/09.txt").context("Error reading input file")?;
@@ -11,22 +12,47 @@ fn main() -> Result<()> {
         })
         .collect::<Result<Vec<_>>>()?;
 
-    for (i, num) in numbers.iter().enumerate().skip(25) {
-        let mut valid = false;
-        'outer: for (j, j_num) in numbers[i - 25..i - 1].iter().enumerate() {
-            for k_num in &numbers[j + 1..i] {
-                if j_num + k_num == *num {
-                    valid = true;
-                    break 'outer;
-                }
-            }
-        }
+    let invalid_num = part1(&numbers)?;
+    println!("Part 1: {}", invalid_num);
+
+    let weakness = part2(&numbers, invalid_num);
+    println!("Part 2: {}", weakness);
+
+    Ok(())
+}
+
+fn part1(numbers: &[usize]) -> Result<usize> {
+    for (i, &num) in numbers.iter().enumerate().skip(25) {
+        let valid = &numbers[i - 25..i]
+            .iter()
+            .combinations(2)
+            .map(|c| c.iter().copied().sum())
+            .any(|s: usize| s == num);
 
         if !valid {
-            println!("{}", num);
-            break;
+            return Ok(num);
         }
     }
 
-    Ok(())
+    Err(anyhow!("No invalid number found"))
+}
+
+fn part2(numbers: &[usize], invalid_num: usize) -> usize {
+    let mut lower = 0;
+    let mut upper = 1;
+    let mut total = numbers[lower] + numbers[upper];
+
+    while total != invalid_num {
+        while total < invalid_num {
+            upper += 1;
+            total += numbers[upper];
+        }
+        while total > invalid_num {
+            total -= numbers[lower];
+            lower += 1;
+        }
+    }
+
+    let slice = &numbers[lower..=upper];
+    slice.iter().min().unwrap() + slice.iter().max().unwrap()
 }
